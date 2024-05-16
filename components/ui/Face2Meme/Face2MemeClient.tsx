3dcc81dea12ed518ebdc24702471698a716f58e5
andreaxricci <62494809+andreaxricci@users.noreply.github.com>
2024-05-17 00:26:35 +0200

    canvas

--- components/ui/Face2Meme/Face2MemeClient.tsx
+++ components/ui/Face2Meme/Face2MemeClient.tsx
@@ -27,6 +27,7 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
   const [error, setError] = useState<string | null>(null);
   const [selectedImage, setSelectedImage] = useState<File | undefined>();
   const [selectedSecondImage, setSelectedSecondImage] = useState<File | undefined>();
+  const [combinedImageUrl, setCombinedImageUrl] = useState<string | null>(null);
   const [text, setText] = useState('');
   const secondImageRef = useRef<HTMLDivElement | null>(null);
   const predictionRef = useRef<HTMLDivElement | null>(null);
@@ -71,11 +72,104 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
     }
   };
 
+  const handleCombinedDownload = () => {
+    if (combinedImageUrl) {
+      saveAs(combinedImageUrl, 'combined_image.jpg');
+    }
+  };
+
+  const generateCombinedImage = () => {
+    if (selectedSecondImage && text) {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        const image = new Image();
+        image.src = URL.createObjectURL(selectedSecondImage);
+
+        image.onload = () => {
+            canvas.width = image.width;
+            canvas.height = image.height;
+
+            if (context) {
+                context.drawImage(image, 0, 0);
+
+                // Calculate font size and line height as a percentage of the image height
+                const fontSize = canvas.height * 0.05; // 5% of image height
+                const lineHeight = fontSize * 1.2; // 120% of the font size for line height
+                const footnoteFontSize = fontSize * 0.6; // 60% of the main font size for the footnote
+                const footnoteLineHeight = footnoteFontSize; // 120% of the footnote font size for line height
+
+                // Adjust maxWidth based on the dimensions of the image
+                let maxWidth;
+                if (canvas.height < canvas.width) {
+                    maxWidth = canvas.height * 0.95;
+                } else {
+                    maxWidth = canvas.width * 0.95;
+                }
+
+                const x = canvas.width / 2; // Center the text horizontally
+                const y_tmp = canvas.height; 
+                const footnoteY = canvas.height - footnoteLineHeight; // Position the footnote at the very bottom
+
+                context.font = `${fontSize}px Arial`; // Dynamic font size
+                context.fillStyle = 'white';
+                context.textAlign = 'center';
+
+                // Function to split text into lines
+                const wrapText = (context: CanvasRenderingContext2D, text: string, x: number, y_tmp: number, maxWidth: number, lineHeight: number) => {
+                    const words = text.split(' ');
+                    let line = '';
+                    const lines = [];
+
+                    for (let n = 0; n < words.length; n++) {
+                        const testLine = line + words[n] + ' ';
+                        const metrics = context.measureText(testLine);
+                        const testWidth = metrics.width;
+                        if (testWidth > maxWidth && n > 0) {
+                            lines.push(line);
+                            line = words[n] + ' ';
+                        } else {
+                            line = testLine;
+                        }
+                    }
+                    lines.push(line);
+
+                    const y = y_tmp - (lineHeight * lines.length) - footnoteLineHeight; // Position the text near the bottom
+
+                    // Calculate the height of the text block
+                    const textBlockHeight = lines.length * lineHeight + footnoteLineHeight * 1.5;
+
+                    // Draw semi-transparent background
+                    context.fillStyle = 'rgba(0, 0, 0, 0.5)'; // Black with 50% opacity
+                    context.fillRect(0, y - lineHeight, canvas.width, textBlockHeight + lineHeight / 2);
+
+                    // Draw the text
+                    context.fillStyle = 'white';
+                    for (let i = 0; i < lines.length; i++) {
+                        context.fillText(lines[i], x, y + i * lineHeight);
+                    }
+                };
+
+                wrapText(context, text, x, y_tmp, maxWidth, lineHeight);
+
+                // Draw the footnote
+                context.font = `${footnoteFontSize}px Arial`; // Smaller font size for footnote
+                context.fillStyle = 'white';
+                context.fillText("Made with AI (www.makethatmeme.com)", x, footnoteY);
+
+                const dataUrl = canvas.toDataURL('image/jpeg', 1.0);
+                setCombinedImageUrl(dataUrl);
+            }
+        };
+    }
+};
+
+
   const removebothImages = () => {
     setSelectedImage(undefined);
     setSelectedSecondImage(undefined);
-    setPrediction(null)
+    setPrediction(null);
     setError(null);
+    setCombinedImageUrl(null);
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -179,6 +273,10 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
   useEffect(() => {
     setMounted(true)
     }, [])
+
+    useEffect(() => {
+      generateCombinedImage();
+    }, [selectedSecondImage, text]);
     
   if (!mounted) {
     return null
@@ -208,11 +306,11 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
           <div >
           
         <div className="md:container md:mx-auto px-4 w-full lg:w-1/2 flex flex-col gap-2 text-foreground mb-4">
-         {/* <div className="mt-8">
+         <div className="mt-8">
             <p className="text-sm text-white sm:text-lg">
             You have { credits } credits
             </p>
-          </div> */}
+          </div> {/*  */}
         <div className="mt-8">
             <p className="text-xl font-bold text-white sm:text-xl">
               Your image
@@ -236,16 +334,7 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
 
           {selectedImage && (
             <div className="mt-4 mb-4 flex flex-col items-center justify-center gap-4 overflow-hidden">
-              {/* <div className="mb-4 w-full">
-                <textarea
-                  className="flex min-h-[60px] w-full  rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
-                  id="memetext"
-                  name="memetext"
-                  placeholder="Add meme text here"
-                  value={text} // Set the value of the textarea to the text state
-                  onChange={(e) => setText(e.target.value)} // Update the text state when the textarea value changes
-                />
-              </div> */}
+              
               
               <div className={styles.imageWrapper}>
 
@@ -253,15 +342,8 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
                   src={URL.createObjectURL(selectedImage)}
                   alt="Thumb"
                 />
-                <div className="absolute inset-0 flex items-center justify-center">
-                  { text && (
-                      <div className="absolute bottom-0 left-0 right-0 p-2 bg-black bg-opacity-50 text-white text-xl">
-                        {text}
-                      </div>
-                  )}    
-                </div>
               </div>
-                
+              
               {!prediction && (
               <div >
                 <Button onClick={removeSelectedImage}>
@@ -295,14 +377,15 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
           )}
 
           {selectedSecondImage && (
-              <div className="mt-4 mb-4 flex flex-col items-center justify-center gap-4 overflow-hidden">
-                <div className={styles.imageWrapper}>
-                  <img className={'${styles.img}'}
-                    src={URL.createObjectURL(selectedSecondImage)}
-                    alt="Thumb"
-                  />
-                </div>
-                <div >
+            <div className="mt-4 mb-4 flex flex-col items-center justify-center gap-4 overflow-hidden">
+              <div className={styles.imageWrapper}>
+                <img className={'${styles.img}'}
+                  src={URL.createObjectURL(selectedSecondImage)}
+                  alt="Thumb"
+                />
+              </div>
+              
+              <div >
                 {!selectedImage && !prediction && (
                   <Button onClick={removeSelectedSecondImage}>
                     Cancel
@@ -325,14 +408,37 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
                   <Button type="submit" disabled>Submit</Button>
                   )}
                 </div>   
-                )}
-                </div>            
+              )}
+              </div>
+
+                <div className="mb-4 w-full">
+                  <textarea
+                    className="flex min-h-[60px] w-full  rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+                    id="memetext"
+                    name="memetext"
+                    placeholder="Add meme text here"
+                    value={text} // Set the value of the textarea to the text state
+                    onChange={(e) => setText(e.target.value)} // Update the text state when the textarea value changes
+                  />
+                </div>
+
+                {combinedImageUrl && (
+                      
+                  <div className={styles.imageWrapper}>
+                  <img className={'${styles.img}'} src={combinedImageUrl} alt="Combined preview" />
+                  </div>
+                        
+                )}  
+                <div>
+                  <Button onClick={handleCombinedDownload}>Download w/ text</Button>
+                </div>     
               </div>
             )}
-        </div>
+            </div>
         </div>
         <div ref={secondImageRef}></div>
       </form>
+      
       </div>
 
       <div ref={predictionRef}></div>
@@ -381,6 +487,16 @@ export default function Face2MemeClient({ user, credits }: Face2MemeClientProps)
                   <Link href="/face2meme">
                       <Button onClick={removebothImages}>Start again</Button>
                     </Link>
+                    </div>
+                    {combinedImageUrl && (
+                      
+                      <div className={styles.imageWrapper}>
+                      <img className={'${styles.img}'} src={combinedImageUrl} alt="Combined preview" />
+                      </div>
+                            
+                    )}  
+                    <div>
+                      <Button onClick={handleCombinedDownload}>Download w/ text</Button>
                     </div> 
                 </div>
               )}
