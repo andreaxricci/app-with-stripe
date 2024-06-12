368c6b9a94d29c0c7bc9d73248eb0c78f7b52d4f
andreaxricci <62494809+andreaxricci@users.noreply.github.com>
2024-06-13 00:13:55 +0200

    generate main page

--- app/generateimage/page.tsx
+++ app/generateimage/page.tsx
@@ -1,11 +1,2 @@
 export const runtime = 'edge'
 
-import MainPage from '@/components/ui/MainPage/MainPage' 
-
-export default async function Home() { 
-  
-    return ( 
-        <MainPage /> 
-        ); 
-  } 
-  
